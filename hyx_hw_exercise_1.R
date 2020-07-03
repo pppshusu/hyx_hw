fe8769b972e3d2aa3474908fc00344326d474ca5
pppshusu <hyx94nb@gmail.com>
2020-07-03 16:46:12 +0800

    Modified hyx_hw_exercise_1.R

--- hyx_hw_exercise_1.R
+++ hyx_hw_exercise_1.R
@@ -1,11 +1,11 @@
-x = 5
-y = 4
-if (x %% 2 == 0 & y %% 2 == 1){
+x <- 5
+y <- 4
+if (x %% 2 == 0 & y %% 2 == 1) {
   stop("error")
-} else if (x > 3 & y <= 3){
+} else if (x > 3 & y <= 3) {
   print("Hello world!")
-} else if (x > 3){
+} else if (x > 3) {
   print ("!dlrow olleH")
-} else if (x <= 3){
+} else if (x <= 3) {
   print ("Something else...")
 }
